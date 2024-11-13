ad0cb2f04c3cb9fdd4b58eebbf9592885a01403a
Daniel Marto <66691323+danncodin@users.noreply.github.com>
2024-11-13 22:57:05 +0000

    adicionado os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 function Home() {
-  return <h1>Samara meu amor, se teu coração palpita pelo meu, me de um beijo encantador. 
-  </h1>
+  return (
+    <h1>
+      Samara meu amor, se teu coração palpita pelo meu, me de um beijo
+      encantador.
+    </h1>
+  );
 }
 
 export default Home;
-
